0efd568380bb749cfe17db2343fb92ef39b50f8a
brstreet2 <83747032+brstreet2@users.noreply.github.com>
2023-01-09 16:15:50 +0700

    Update UserModel.java

--- app/src/main/java/com/example/layarkaryadev/Model/UserModel.java
+++ app/src/main/java/com/example/layarkaryadev/Model/UserModel.java
@@ -3,6 +3,10 @@ package com.example.layarkaryadev.Model;
 public class UserModel {
     public String fullName, lastName, email;
 
+    public int movieWatched;
+
+    public int contentCount;
+
     public String getFullName() {
         return fullName;
     }
@@ -37,15 +41,33 @@ public class UserModel {
 
     public int coin;
 
+    public int getContentCount() {
+        return contentCount;
+    }
+
+    public void setContentCount(int contentCount) {
+        this.contentCount = contentCount;
+    }
+
+    public int getMovieWatched() {
+        return movieWatched;
+    }
+
+    public void setMovieWatched(int movieWatched) {
+        this.movieWatched = movieWatched;
+    }
+
     public UserModel(){
 
     }
 
-    public UserModel(String fullName, String lastName, String email, int coin){
+    public UserModel(String fullName, String lastName, String email, int coin, int contentCount, int movieWatched){
         this.fullName = fullName;
         this.lastName = lastName;
         this.email = email;
         this.coin = coin;
+        this.contentCount = contentCount;
+        this.movieWatched = movieWatched;
     }
 
 }
